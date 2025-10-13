eee87e5d0d028da4680f9b9c7af9d7e6b8397a64
Niels Neerhoff <40208446+nielsneerhoff@users.noreply.github.com>
2025-10-13 12:01:38 +0200

    Resource for resolving node/IO FQN (#130)

---
+++ packages/ordeq/tests/resources/runner/run_rename_run.py
@@ -0,0 +1,13 @@
+from ordeq import run, node
+
+
+@node
+def print_message():
+    print("Hello from printer")
+
+
+run(print_message, verbose=True)
+
+show_message = print_message
+
+run(show_message, verbose=True)
