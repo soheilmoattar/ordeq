803aa3d2ce1ea0943ede9f9bfa931a0bcf763691
danieldutu <56995918+danieldutu@users.noreply.github.com>
2025-10-23 12:10:41 +0300

    `ordeq-numpy`: Add Memory-Mapped Array Loading Support to NumpyBinary (#166)

--- packages/ordeq-numpy/src/ordeq_numpy/binary_array.py
+++ packages/ordeq-numpy/src/ordeq_numpy/binary_array.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any
 
 import numpy as np
 from ordeq import IO
@@ -24,9 +25,18 @@ class NumpyBinary(IO[np.ndarray]):
 
     path: PathLike
 
-    def load(self) -> np.ndarray:
+    def load(self, **load_options: Any) -> np.ndarray:
+        """Load numpy array with optional parameters.
+
+        Args:
+            **load_options: Arguments passed to np.load()
+                (e.g., mmap_mode, allow_pickle, max_header_size)
+
+        Returns:
+            Numpy array
+        """
         with self.path.open("rb") as fh:
-            return np.load(fh)
+            return np.load(fh, **load_options)
 
     def save(self, array: np.ndarray) -> None:
         with self.path.open("wb") as fh:
